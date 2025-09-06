b0cb17625fb281e3d3724913c47d45eafb972390
vitordsrodrigues <165427238+rodriguessilvavitor@users.noreply.github.com>
2025-09-05 23:05:49 -0300

    cadastro de pet concluido

--- frontend/src/componentes/form/PetForm.js
+++ frontend/src/componentes/form/PetForm.js
@@ -2,24 +2,42 @@ import { useState } from "react"
 import formStyles from './form.module.css'
 import Input from "./Input"
 import Select from "./Select"
+import { set } from "mongoose"
 function PetForm({handleSubmit,petData,btnText}){
     const [pet,setPet] = useState(petData || {})
     const [preview,setPreview] = useState([])
     const colors = ["branco", "preto", "cinza", "marrom", "caramelo"]
 
     function onFileChange(e){
-
+        setPreview(Array.from(e.target.files))
+        setPet({...pet, images: [...e.target.files]})
     }
 
     function handleChange(e){
+        setPet({...pet, [e.target.name]: e.target.value})
     }
 
     function handleColor(e){
-        
+        setPet({...pet, color: e.target.options[e.target.selectedIndex].text })
+    }
+
+    function submit(e){
+        e.preventDefault()
+        console.log(pet)
+        handleSubmit(pet)
     }
 
     return(
-        <form className={formStyles.form_container}>
+        <form onSubmit={submit} className={formStyles.form_container}>
+            <div className={formStyles.preview_pet_images}>
+                {preview.length > 0
+                    ? preview.map((image, index) =>
+                        <img src={URL.createObjectURL(image)} alt={pet.name} key={`${pet.name}+${index}`} />
+                    ) : pet.images && pet.images.map((image, index)=>(
+                        <img src={`${process.env.REACT_APP_API}`} alt={pet.name} key={`${pet.name}+${index}`} />
+                    ))
+                }
+            </div>
             <Input 
                text='Imagens do Pet'
                type='file'
--- frontend/src/componentes/pages/Pet/addPet.js
+++ frontend/src/componentes/pages/Pet/addPet.js
@@ -1,14 +1,51 @@
 import styles from './AddPet.module.css'
 import api from '../../../utils/api'
 import { useState } from 'react'
-import {useHistory} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 /*componentes*/
 import PetForm from '../../form/PetForm'
 
 /*hooks*/
 import useFlashMessage from '../../../hooks/useFlashMessage'
+
+
 function AddPet(){
+    const [token] = useState(localStorage.getItem('token')|| '')
+    const {setFlashMessage} = useFlashMessage()
+    const navigate = useNavigate()
+
+    async function registerPet(pet) {
+        let msgType = 'success'
+
+        const formData = new FormData
+
+        await Object.keys(pet).forEach((key)=>{
+            if(key ==='images'){
+                for(let i = 0; i < pet[key].length; i++){
+                    formData.append('images', pet[key][i])
+                }
+            } else{
+                formData.append(key, pet[key])
+            }
+        })
+
+        const data = await api.post('pets/create', formData, {
+            Authorization: `Bearer ${JSON.parse(token)}`,'Content-Type': 'multipart/form-data'
+        }).then((response)=>{
+            return response.data
+        })
+        .catch((err) => {
+            msgType = 'error'
+            return err.response.data
+        })
+        setFlashMessage(data.message, msgType)
+        
+        if(msgType !== 'error') {
+            navigate('/pets/mypets')
+        }
+    }
+
     return(
         <section className={styles.addpet_header}>
             <div>
@@ -16,7 +53,7 @@ function AddPet(){
                 <p>Depois ele ficara disponivel para adoção</p>
             </div>
     
-            <PetForm btnText="Cadastrar Pet" />
+            <PetForm  handleSubmit={registerPet} btnText="Cadastrar Pet" />
         </section>
     )
 }
